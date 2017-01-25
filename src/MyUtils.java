0bd6a696682a183f55abe92a0e90f1026d3735dc
Sopheak Hang <hangsopheak.hod@gmail.com>
2017-01-25 21:50:02 +0700

    first commit

---
+++ src/MyUtils.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.Set;
+
+public class MyUtils{
+	
+	public static void checkDuplicate(Set<String> s, String[] countries) {
+	   s.addAll(Arrays.asList(countries));
+	   System.out.printf("Set type = %s ", s.getClass().getName());
+	   System.out.println(s);
+	}
+}
